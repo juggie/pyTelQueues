c0ba111e03adc470e619eaca46e6f0f7e13f2d35
Paul Belanger <paul.belanger@polybeacon.com>
2013-05-01 00:39:40 -0400

    Replace tabbing with spacing
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- FastAGIQueues/config.py
+++ FastAGIQueues/config.py
@@ -2,32 +2,32 @@
 import ConfigParser, platform
 
 class Config():
-	def __init__(self, logger, configfile = 'FastAGIQueues.cfg'):
-		#store class input
-		self._logger, self._configfile = (logger, configfile)
+    def __init__(self, logger, configfile = 'FastAGIQueues.cfg'):
+        #store class input
+        self._logger, self._configfile = (logger, configfile)
 
-		self._config = ConfigParser.ConfigParser()
-		self._config.read(self._configfile)
-		self.fastagi_port = self.read_config_var('fastagi', 'port', 4573, 'int')
-		self.redisinstancechannel = self.read_config_var('redis', 'instancechannel', 'agiqueues.%s' % platform.node(), 'str')
-		self.redisglobalchannel = self.read_config_var('redis', 'globalchannel', 'agiqueues.global', 'str')
-		self.redisip = self.read_config_var('global', 'redisip', '192.168.99.20', 'str') #should be localhost 
-		self.redisport = self.read_config_var('global', 'redisport', 6379, 'int')
+        self._config = ConfigParser.ConfigParser()
+        self._config.read(self._configfile)
+        self.fastagi_port = self.read_config_var('fastagi', 'port', 4573, 'int')
+        self.redisinstancechannel = self.read_config_var('redis', 'instancechannel', 'agiqueues.%s' % platform.node(), 'str')
+        self.redisglobalchannel = self.read_config_var('redis', 'globalchannel', 'agiqueues.global', 'str')
+        self.redisip = self.read_config_var('global', 'redisip', '192.168.99.20', 'str') #should be localhost 
+        self.redisport = self.read_config_var('global', 'redisport', 6379, 'int')
 
-		logger.Message('Config loaded', 'CONFIG')
+        logger.Message('Config loaded', 'CONFIG')
 
-	def defaulting(self, section, variable, default, quiet = False):
-		if quiet == False:
-			self._logger.Message(str(variable) + ' not set in ['+str(section)+'] defaulting to: \''+str(default)+'\'', 'CONFIG')
+    def defaulting(self, section, variable, default, quiet = False):
+        if quiet == False:
+            self._logger.Message(str(variable) + ' not set in ['+str(section)+'] defaulting to: \''+str(default)+'\'', 'CONFIG')
 
-	def read_config_var(self, section, variable, default, type = 'str', quiet = False):
-		try:
-			if type == 'str':
-				return self._config.get(section,variable)
-			elif type == 'bool':
-				return self._config.getboolean(section,variable)
-			elif type == 'int':
-				return int(self._config.get(section,variable))
-		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			self.defaulting(section, variable, default, quiet)
-			return default
+    def read_config_var(self, section, variable, default, type = 'str', quiet = False):
+        try:
+            if type == 'str':
+                return self._config.get(section,variable)
+            elif type == 'bool':
+                return self._config.getboolean(section,variable)
+            elif type == 'int':
+                return int(self._config.get(section,variable))
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            self.defaulting(section, variable, default, quiet)
+            return default
--- FastAGIQueues/fastagi.py
+++ FastAGIQueues/fastagi.py
@@ -1,146 +1,146 @@
 import asyncore, asynchat, socket, json, hashlib
 
 class FastAGIServer(asyncore.dispatcher):
-	def __init__(self, logger, config, redis):
-		asyncore.dispatcher.__init__(self)
-		#store class input
-		self._logger, self._config, self._redis = (logger, config, redis)
+    def __init__(self, logger, config, redis):
+        asyncore.dispatcher.__init__(self)
+        #store class input
+        self._logger, self._config, self._redis = (logger, config, redis)
 
-		#clients
-		self._clients = {}
+        #clients
+        self._clients = {}
 
-		#asynccore
-		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.set_reuse_addr()
-		self.bind(("", self._config.fastagi_port))
-		self.listen(5)
-		self._logger.Message('AGI Queue Server listening on port: %i' % self._config.fastagi_port, 'FASTAGI')		
+        #asynccore
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(("", self._config.fastagi_port))
+        self.listen(5)
+        self._logger.Message('AGI Queue Server listening on port: %i' % self._config.fastagi_port, 'FASTAGI')
 
-		#redis
-		self._redis_subid = self._redis.subscribe(config.redisinstancechannel) #return subid to main for loop
+        #redis
+        self._redis_subid = self._redis.subscribe(config.redisinstancechannel) #return subid to main for loop
 
-	def subid(self):
-		return self._redis_subid
+    def subid(self):
+        return self._redis_subid
 
-	def handle_accept(self):
-		pair = self.accept()
-		if pair is None:
-			pass
-		else:
-			sock, addr = pair
-			handler = FAGIChannel(sock, addr, self._logger, self._clients, self._redis, self._config)
-	
-	def numclients(self):
-		return len(self)
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is None:
+            pass
+        else:
+            sock, addr = pair
+            handler = FAGIChannel(sock, addr, self._logger, self._clients, self._redis, self._config)
 
-	def getclient(self, clientMD5):
-		return self._clients[clientMD5]
+    def numclients(self):
+        return len(self)
+
+    def getclient(self, clientMD5):
+        return self._clients[clientMD5]
 
 class FAGIChannel(asynchat.async_chat):
-	def __init__(self, sock, addr, logger, clients, redis, config):
-		#store class input
-		self._logger, self._clients, self._redis, self._config = (logger, clients, redis, config)
-		
-		asynchat.async_chat.__init__(self, sock)
-		#fagi terminator
-		self.set_terminator("\n")
-		#buffer for asynchat
-		self._buffer = []
-		#ip/port in string
-		self._straddr = str(addr)
-		#did we complete the initial agi connection
-		self._connected = False
-		#is moh enabled
-		self._moh = False
-		
-		#set md5 id of connected client
-		self._clientMD5 = hashlib.md5(str(addr)).hexdigest()
+    def __init__(self, sock, addr, logger, clients, redis, config):
+        #store class input
+        self._logger, self._clients, self._redis, self._config = (logger, clients, redis, config)
 
-		self._clients[self._clientMD5]=self
+        asynchat.async_chat.__init__(self, sock)
+        #fagi terminator
+        self.set_terminator("\n")
+        #buffer for asynchat
+        self._buffer = []
+        #ip/port in string
+        self._straddr = str(addr)
+        #did we complete the initial agi connection
+        self._connected = False
+        #is moh enabled
+        self._moh = False
 
-		logger.Message('Incoming FastAGI connection from %s' % repr(addr), 'FASTAGI')
-		#logger.Message('%i Connected FastAGI sessions' % len(CONNECTEDCLIENTS), 'FASTAGI')
+        #set md5 id of connected client
+        self._clientMD5 = hashlib.md5(str(addr)).hexdigest()
 
-	def handle_redis_event(self, event):
-		self._logger.Message('Event %s' % event, 'REDIS')
-		if event['event']=='answer':
-			self.AGI_Answer()
-		elif event['event']=='playback':
-			self.AGI_Playback(str(event['parameters'])) #confused as to why str() is necessairy 
-		elif event['event']=='mohon':
-			self.AGI_MusicOnHold(True) 
-		elif event['event']=='mohoff':
-			self.AGI_MusicOnHold(False) 
-		elif event['event']=='hangup':
-			self.AGI_Hangup() 
+        self._clients[self._clientMD5]=self
 
-	def send_redis_event(self, event):
-		tosend = json.dumps({'event' : event, 'clientMD5' : self._clientMD5,'instance_channel' : self._config.redisinstancechannel})
-		self._redis.publish(self._config.redisglobalchannel, tosend) #move to config
-		self._logger.Message('Sent %s on channel: %s' % (tosend, self._config.redisglobalchannel), 'REDIS') #move to config
+        logger.Message('Incoming FastAGI connection from %s' % repr(addr), 'FASTAGI')
+        #logger.Message('%i Connected FastAGI sessions' % len(CONNECTEDCLIENTS), 'FASTAGI')
 
-	def send_command(self, data):
-		self._logger.Message('SENT: %s' % data, 'AGI')
-		self.push(data+'\n')
-		
-	def collect_incoming_data(self, data):
-		# Append incoming data to the buffer
-		self._buffer.append(data)
+    def handle_redis_event(self, event):
+        self._logger.Message('Event %s' % event, 'REDIS')
+        if event['event']=='answer':
+            self.AGI_Answer()
+        elif event['event']=='playback':
+            self.AGI_Playback(str(event['parameters'])) #confused as to why str() is necessairy 
+        elif event['event']=='mohon':
+            self.AGI_MusicOnHold(True)
+        elif event['event']=='mohoff':
+            self.AGI_MusicOnHold(False)
+        elif event['event']=='hangup':
+            self.AGI_Hangup()
 
-	def found_terminator(self):
-		line = "".join(self._buffer)
-		self._buffer = []
-		self.handle_line(line)
+    def send_redis_event(self, event):
+        tosend = json.dumps({'event' : event, 'clientMD5' : self._clientMD5,'instance_channel' : self._config.redisinstancechannel})
+        self._redis.publish(self._config.redisglobalchannel, tosend) #move to config
+        self._logger.Message('Sent %s on channel: %s' % (tosend, self._config.redisglobalchannel), 'REDIS') #move to config
 
-	def handle_line(self, line):
-		if self._connected == False:
-			if line == '':
-				self._connected = True
-				self._logger.Message('Initial Variables Received', 'AGI')
-				self.send_redis_event('ring')
-			else:
-				self._logger.Message('Variable -> %s' % line, 'AGI')
-		else:
-			self._logger.Message('Received %s' % line, 'AGI')
-			self.HandleCall(line)
-			
-	def handle_close(self):
-		self._logger.Message('FastAGI connection from %s closed' % self._straddr, 'AGI')
-		if self._clientMD5 in self._clients: del self._clients[self._clientMD5]
-		self.close()
+    def send_command(self, data):
+        self._logger.Message('SENT: %s' % data, 'AGI')
+        self.push(data+'\n')
 
-	def handle_errorr(self):
-		self._logger.Message('ERROR: FastAGI connection from %s closed' % self._straddr, 'AGI')
-		if self._clientMD5 in self._clients: del self._clients[self._clientMD5]
-		self.close()
-	
-	#parsing of agi responses goes here
-	def HandleCall(self,line):
-		if line[:3] == '200':
-			self.send_redis_event('ok')
-		elif line[:3] == '510':
-			self.send_redis_event('invalid')
-		elif line[:3] == '511':
-			self.send_redis_event('dead')
-		elif line == 'HANGUP':
-			self.send_redis_event('hangup')
-			self.handle_close()
-		else:
-			self._logger.Message('Unknown event: %s' % line, 'AGI')
-			
-	def AGI_Answer(self):
-		self.send_command('ANSWER')
+    def collect_incoming_data(self, data):
+        # Append incoming data to the buffer
+        self._buffer.append(data)
 
-	def AGI_Playback(self, file='beep'):
-		self.send_command('STREAM FILE %s \'\' 0' % file)
-		
-	def AGI_MusicOnHold(self, moh_state, moh_class = ''):
-		if moh_state == True:
-			self._moh = True 
-			self.send_command('SET MUSIC ON %s' % moh_class)
-		else:
-			self._moh = False
-			self.send_command('SET MUSIC OFF')
+    def found_terminator(self):
+        line = "".join(self._buffer)
+        self._buffer = []
+        self.handle_line(line)
 
-	def AGI_Hangup(self):
-		self.send_command('HANGUP')
+    def handle_line(self, line):
+        if self._connected == False:
+            if line == '':
+                self._connected = True
+                self._logger.Message('Initial Variables Received', 'AGI')
+                self.send_redis_event('ring')
+            else:
+                self._logger.Message('Variable -> %s' % line, 'AGI')
+        else:
+            self._logger.Message('Received %s' % line, 'AGI')
+            self.HandleCall(line)
+
+    def handle_close(self):
+        self._logger.Message('FastAGI connection from %s closed' % self._straddr, 'AGI')
+        if self._clientMD5 in self._clients: del self._clients[self._clientMD5]
+        self.close()
+
+    def handle_errorr(self):
+        self._logger.Message('ERROR: FastAGI connection from %s closed' % self._straddr, 'AGI')
+        if self._clientMD5 in self._clients: del self._clients[self._clientMD5]
+        self.close()
+
+    #parsing of agi responses goes here
+    def HandleCall(self,line):
+        if line[:3] == '200':
+            self.send_redis_event('ok')
+        elif line[:3] == '510':
+            self.send_redis_event('invalid')
+        elif line[:3] == '511':
+            self.send_redis_event('dead')
+        elif line == 'HANGUP':
+            self.send_redis_event('hangup')
+            self.handle_close()
+        else:
+            self._logger.Message('Unknown event: %s' % line, 'AGI')
+
+    def AGI_Answer(self):
+        self.send_command('ANSWER')
+
+    def AGI_Playback(self, file='beep'):
+        self.send_command('STREAM FILE %s \'\' 0' % file)
+
+    def AGI_MusicOnHold(self, moh_state, moh_class = ''):
+        if moh_state == True:
+            self._moh = True
+            self.send_command('SET MUSIC ON %s' % moh_class)
+        else:
+            self._moh = False
+            self.send_command('SET MUSIC OFF')
+
+    def AGI_Hangup(self):
+        self.send_command('HANGUP')
